0c094155bb84da4af40398052fa31a5118cce995
Katerina Pilatova <kpilatov@redhat.com>
2017-10-17 18:37:51 +0200

    autoscaling: added signal handlers

--- playbooks/provisioning/openstack/autoscaling/autoscaling.py
+++ playbooks/provisioning/openstack/autoscaling/autoscaling.py
@@ -1,12 +1,27 @@
 import argparse
 import shade
+import signal
+import sys
+
+
+# Handle SIGINT, SIGTERM
+def handler(signum, frame):
+
+    name= "SIGTERM" if signum == 15 else "SIGINT"
+    print('\n{} received, closing program.'.format(name))
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, handler)
+signal.signal(signal.SIGTERM, handler)
+
 
 class AutoScaling:
     ''' This class implements the autoscaling service.
         Its main method runs in the background, gathers metrics and triggers
         scaling events when certain limits are met. '''
 
-    check_history = [False, False, False]
+    check_history = [False, False, False] # True = workload exceeded
+    interval = 1 # in minutes, describes how often a check is performed
 
     def AutoScaling(self):
         pass
@@ -44,13 +59,15 @@ class AutoScaling:
     def run_prototype(self):
         ''' Perform checks every minute.
             If CPU workload exceeds 70% 3 times in a row, scale up by 1. '''
-        pass
+        while True:
+            pass
 
 
 if __name__ == "__main__":
 
     # Parse arguments
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--interval', type=int, default=1)
 
     # Create and run an autoscaling service
     service = AutoScaling()
